933bdd7c5e2c9be3620d14d22c41c3ea635e83ae
20250102 <ksoung140w@gmail.com>
2025-01-02 14:33:58 +0900

    first commit

--- src/main/java/com/blog/react_spring/ReactSpringApplication.java
+++ src/main/java/com/blog/react_spring/ReactSpringApplication.java
@@ -2,8 +2,10 @@ package com.blog.react_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+//@SpringBootApplication
 public class ReactSpringApplication {
 
 	public static void main(String[] args) {
